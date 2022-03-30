3ac872140b390e53d8b4019f8557d6601a88f7d8
Alexander B <bas2784@gmail.com>
2022-03-30 10:55:39 +0300

    промежуточные записи

--- 2.1-databases/models_list_displaying/books/views.py
+++ 2.1-databases/models_list_displaying/books/views.py
@@ -1,7 +1,47 @@
-from django.shortcuts import render
+from datetime import datetime
+
+from django.core.paginator import Paginator
+from django.shortcuts import render, redirect
+
+from books.models import Book
+
+
+def index(request):
+    return redirect('books')
 
 
 def books_view(request):
     template = 'books/books_list.html'
-    context = {}
+    books = Book.objects.all()
+    context = {'books': books}
+    return render(request, template, context)
+
+
+def show_book(request, dt: datetime):
+    template = 'books/book.html'
+    books_objects = Book.objects.all()
+    book_date = [b_date.pub_date for b_date in books_objects]
+    book = Book.objects.get(pub_date=dt)
+    get_index = book_date.index(book.pub_date)
+
+    previous = None
+    next_p = None
+    if get_index == 0:
+        previous = None
+        next_p = get_index + 1
+    elif book.pub_date == book_date[-1]:
+        previous = get_index - 1
+        next_p = None
+    else:
+        next_p = get_index + 1
+        previous = get_index - 1
+
+    context = {
+        'book': book,
+        'book_date': book_date,
+        'next': next_p,
+        'previous': previous,
+        'next_date': book_date[next_p],
+        'previous_date': book_date[previous],
+    }
     return render(request, template, context)
--- 2.1-databases/models_list_displaying/main/urls.py
+++ 2.1-databases/models_list_displaying/main/urls.py
@@ -13,12 +13,31 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
+from datetime import datetime
 
-from books.views import books_view
+from django.contrib import admin
+from django.urls import path, register_converter
+
+from books.views import books_view, show_book, index
+
+
+class DateConverter:
+    regex = r'[0-9]{4}-[0-9]{2}-[0-9]{2}'
+    format = '%Y-%m-%d'
+
+    def to_python(self, value: str) -> datetime:
+        return datetime.strptime(value, self.format)
+
+    def to_url(self, value):
+        return value
+
+
+register_converter(DateConverter, 'date')
+
 
 urlpatterns = [
-    path('', books_view, name='books'),
+    path('', index),
+    path('books/', books_view, name='books'),
     path('admin/', admin.site.urls),
+    path('books/<date:dt>', show_book),
 ]
