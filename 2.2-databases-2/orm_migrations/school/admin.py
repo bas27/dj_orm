abd7acfee5695c4b2e6debf3348082af2d991156
Alexander B <bas2784@gmail.com>
2022-03-31 14:30:42 +0300

    Внесены изменения по заданию

--- 2.2-databases-2/orm_migrations/school/admin.py
+++ 2.2-databases-2/orm_migrations/school/admin.py
@@ -2,12 +2,23 @@ from django.contrib import admin
 
 from .models import Student, Teacher
 
+class StudentInline(admin.TabularInline):
+    model = Student.teacher.through
+    extra = 0
 
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    pass
-
+    list_display = ['id', 'name', 'group']
+    list_filter = ['group']
+    inlines = [
+        StudentInline,
+    ]
+    exclude = ('teacher',)
 
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
-    pass
+    list_display = ['id', 'name', 'subject']
+    list_filter = ['subject']
+    inlines = [
+        StudentInline,
+    ]
--- 2.2-databases-2/orm_migrations/website/settings.py
+++ 2.2-databases-2/orm_migrations/website/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'school',
 ]
 
@@ -47,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'website.urls'
@@ -75,7 +77,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'netology_orm_migrations',
         'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'PORT': '5433',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
     }
 }
 
@@ -102,7 +106,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.1/topics/i18n/
 
-LANGUAGE_CODE = 'RU-ru'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'Europe/Moscow'
 
@@ -130,4 +134,6 @@ try:
     from .settings_local import *
 except ImportError:
     pass
+
+INTERNAL_IPS = ['127.0.0.1']
 
